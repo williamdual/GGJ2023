0b5bb7c34c844d1d06340ade9687fd931f93a789
williamdual <williamdual115@gmail.com>
2023-02-05 13:22:23 -0500

    update volumes

--- Project/Scripts/GameManager.gd
+++ Project/Scripts/GameManager.gd
@@ -110,18 +110,18 @@ func _on_TurnTimer_timeout():
 	startTurn()
 
 func playPlacementSound():
-	$AudioStreamPlayer.volume_db = -4
+	$AudioStreamPlayer.volume_db = 4
 	$AudioStreamPlayer.stream = placementSound
 	$AudioStreamPlayer.play()
 func playNewRoundSound():
-	$AudioStreamPlayer.volume_db = -24
+	$AudioStreamPlayer.volume_db = 0
 	$AudioStreamPlayer.stream = newRoundSound
 	$AudioStreamPlayer.play()
 func playSelectElementSound():
-	$AudioStreamPlayer.volume_db = 0
+	$AudioStreamPlayer.volume_db = 6
 	$AudioStreamPlayer.stream = selectElementSound
 	$AudioStreamPlayer.play()
 func playEvolveSound():
-	$EvolutionStreamPlayer.volume_db = 0
+	$EvolutionStreamPlayer.volume_db = 6
 	$EvolutionStreamPlayer.stream = evolveSound
 	$EvolutionStreamPlayer.play()
